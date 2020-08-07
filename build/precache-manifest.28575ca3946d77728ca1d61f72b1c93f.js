7ec3a7997149a2b600358c58e634094fc23c972c
rickydamarsaputra <retrocode.rc@gmail.com>
2020-08-07 15:00:51 +0700

    change logo bpipi

---
+++ build/precache-manifest.28575ca3946d77728ca1d61f72b1c93f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b671fa1cc84bda21cf449899992e41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca75ad49c43f19f7bd51",
+    "url": "/static/css/main.97150f9b.chunk.css"
+  },
+  {
+    "revision": "7f3829abea1d6e660a33",
+    "url": "/static/js/2.634d5b2b.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.634d5b2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca75ad49c43f19f7bd51",
+    "url": "/static/js/main.e58f6676.chunk.js"
+  },
+  {
+    "revision": "703c7a45be9788469ff1",
+    "url": "/static/js/runtime-main.d8553429.js"
+  },
+  {
+    "revision": "2654f3d02fea9fb9c43491c40391a996",
+    "url": "/static/media/bpipi-bg-text-revisi.2654f3d0.png"
+  },
+  {
+    "revision": "8db4e85f6ff3906c389ac7e43ed34a36",
+    "url": "/static/media/shoes.8db4e85f.png"
+  }
+]);
